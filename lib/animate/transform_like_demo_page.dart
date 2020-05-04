b3e175e914f27beb48a6ba75aa274bc6f320ee99
felixyzhang <felixyzhang@tencent.com>
2020-05-04 19:02:09 +0800

    facebook like anim demo
    长按动画

--- lib/animate/transform_like_demo_page.dart
+++ lib/animate/transform_like_demo_page.dart
@@ -28,6 +28,7 @@ class FbReaction extends StatefulWidget {
 class _FbReactionState extends State<FbReaction> with TickerProviderStateMixin {
   bool _isLongPress = false;
   bool _isLike = false;
+  bool _isDragging = false;
   Timer holdTimer;
   Duration durationLongPress = Duration(milliseconds: 250);
   final int durationAnimationBox = 500;
@@ -36,13 +37,25 @@ class _FbReactionState extends State<FbReaction> with TickerProviderStateMixin {
   AnimationController _animBtnShortPressController;
   Animation _zoomIconLikeInBtnShort, _tiltIconLikeInBtnShort;
 
+  // For long press btn
+  AnimationController _animControlBtnLongPress, animControlBox;
+  Animation zoomIconLikeInBtn, tiltIconLikeInBtn, zoomTextLikeInBtn;
+  Animation fadeInBox;
+
   AudioPlayer _audioPlayer;
+
+  // 0 = nothing, 1 = like, 2 = love, 3 = haha, 4 = wow, 5 = sad, 6 = angry
+  int whichIconUserChoose = 0;
+
+  // 0 = nothing, 1 = like, 2 = love, 3 = haha, 4 = wow, 5 = sad, 6 = angry
+  int currentIconFocus = 0;
 
   @override
   void initState() {
     super.initState();
     _audioPlayer = AudioPlayer();
 
+    //for short press
     _animBtnShortPressController =
         AnimationController(vsync: this, duration: Duration(milliseconds: 500));
     _zoomIconLikeInBtnShort =
@@ -55,66 +68,179 @@ class _FbReactionState extends State<FbReaction> with TickerProviderStateMixin {
     _tiltIconLikeInBtnShort.addListener(() {
       setState(() {});
     });
+
+    //for long press
+    _animControlBtnLongPress =
+        AnimationController(vsync: this, duration: Duration(milliseconds: 150));
+    zoomIconLikeInBtn = Tween(begin: 1.0, end: 0.85).animate(_animControlBtnLongPress);
+    tiltIconLikeInBtn = Tween(begin: 0.0, end: 0.2).animate(_animControlBtnLongPress);
+    zoomTextLikeInBtn = Tween(begin: 1.0, end: 0.85).animate(_animControlBtnLongPress);
+
+    zoomIconLikeInBtn.addListener(() {
+      setState(() {});
+    });
+    tiltIconLikeInBtn.addListener(() {
+      setState(() {});
+    });
+    zoomTextLikeInBtn.addListener(() {
+      setState(() {});
+    });
+
+    animControlBox = AnimationController(
+        vsync: this, duration: Duration(milliseconds: durationAnimationBox));
+    fadeInBox = Tween(begin: 0.0, end: 1.0).animate(
+        CurvedAnimation(parent: animControlBox, curve: Interval(0.7, 1.0)));
+    fadeInBox.addListener(() {
+      setState(() {});
+    });
   }
 
   @override
   void dispose() {
     _animBtnShortPressController.dispose();
+    _animControlBtnLongPress.dispose();
+    animControlBox.dispose();
     _audioPlayer.dispose();
     super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-        child: GestureDetector(
-      onTap: onTapBtn,
-      onTapDown: onTapDownBtn,
-      onTapUp: onTapUpBtn,
+    return GestureDetector(
+        child: Column(children: <Widget>[
+      Container(
+        width: double.infinity,
+        height: 100,
+      ),
+      Container(
+          margin: EdgeInsets.only(left: 20, right: 20),
+          width: double.infinity,
+          height: 350,
+          child: Stack(children: <Widget>[
+            Stack(
+              alignment: Alignment.bottomCenter,
+              children: <Widget>[buildBox(), buildIcons()],
+            ),
+            buildBtnLike()
+          ])),
+    ]));
+  }
+
+  Widget buildBox() {
+    return Opacity(
+      opacity: this.fadeInBox.value,
       child: Container(
-        width: 100,
+        width: 300,
+        height: 50,
+        margin: EdgeInsets.only(bottom: 130, left: 10),
         decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(4.0),
             color: Colors.white,
-            border: Border.all(color: getColorBorderBtn())),
+            borderRadius: BorderRadius.circular(30.0),
+            border: Border.all(color: Colors.grey[300], width: 0.3),
+            boxShadow: [
+              BoxShadow(
+                  color: Colors.grey,
+                  blurRadius: 5.0,
+                  // LTRB
+                  offset: Offset.lerp(Offset(0.0, 0.0), Offset(0.0, 0.5), 10.0))
+            ]),
+      ),
+    );
+  }
+
+  Widget buildIcons() {
+    return Container(
+      width: 300.0,
+      height: 250.0,
+      margin: EdgeInsets.only(left: 0, top: 50.0),
+      child: Row(
+        children: [
+          Container(
+            margin: EdgeInsets.only(bottom: 60),
+            width: 40.0,
+            height: 40,
+            child: Column(
+              children: <Widget>[
+                currentIconFocus == 1
+                    ? Container(
+                        child: Text(
+                          "Like",
+                          style: TextStyle(fontSize: 8.0, color: Colors.white),
+                        ),
+                        decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(10.0),
+                            color: Colors.black.withOpacity(0.3)),
+                        padding: EdgeInsets.only(
+                            left: 7.0, right: 7.0, top: 2.0, bottom: 2.0),
+                        margin: EdgeInsets.only(bottom: 8.0),
+                      )
+                    : Container(),
+                Image.asset(
+                  'assets/like.gif',
+                  width: 40.0,
+                  height: 40.0,
+                  fit: BoxFit.contain,
+                ),
+              ],
+            ),
+          )
+        ],
+      ),
+    );
+  }
+
+  Widget buildBtnLike() {
+    return Container(
+      child: GestureDetector(
+        onTap: onTapBtn,
+        onTapDown: onTapDownBtn,
+        onTapUp: onTapUpBtn,
         child: Container(
-          padding: EdgeInsets.all(10),
-          color: Colors.transparent,
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: <Widget>[
-              Transform.scale(
-                child: Transform.rotate(
-                  child: Image.asset(getImageIconBtn(),
-                      height: 25, width: 25, fit: BoxFit.contain),
-                  angle: !_isLongPress
-                      ? handleOutputRnageTiltIconLike(
-                          _tiltIconLikeInBtnShort.value)
-                      : 0,
+          child: Container(
+            padding: EdgeInsets.all(10),
+            color: Colors.transparent,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: <Widget>[
+                Transform.scale(
+                  child: Transform.rotate(
+                    child: Image.asset(getImageIconBtn(),
+                        height: 25, width: 25, fit: BoxFit.contain),
+                    angle: !_isLongPress
+                        ? handleOutputRnageTiltIconLike(
+                            _tiltIconLikeInBtnShort.value)
+                        : tiltIconLikeInBtn.value,
+                  ),
+                  scale: !_isLongPress
+                      ? handleOutputRnageZoomInIconLike(
+                          _zoomIconLikeInBtnShort.value)
+                      : zoomIconLikeInBtn.value,
                 ),
-                scale: !_isLongPress
-                    ? handleOutputRnageZoomInIconLike(
-                        _zoomIconLikeInBtnShort.value)
-                    : 0,
-              ),
-              Transform.scale(
-                child: Text(
-                  "Like",
-                  style: TextStyle(
-                      color: getColorTextBtn(),
-                      fontSize: 14,
-                      fontWeight: FontWeight.bold),
-                ),
-                scale: !_isLongPress
-                    ? handleOutputRnageZoomInIconLike(
-                        _zoomIconLikeInBtnShort.value)
-                    : 0,
-              )
-            ],
+                Transform.scale(
+                  child: Text(
+                    "Like",
+                    style: TextStyle(
+                        color: getColorTextBtn(),
+                        fontSize: 14,
+                        fontWeight: FontWeight.bold),
+                  ),
+                  scale: !_isLongPress
+                      ? handleOutputRnageZoomInIconLike(
+                          _zoomIconLikeInBtnShort.value)
+                      : zoomTextLikeInBtn.value,
+                )
+              ],
+            ),
           ),
         ),
       ),
-    ));
+      width: 100,
+      margin: EdgeInsets.only(top: 190),
+      decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(4.0),
+          color: Colors.white,
+          border: Border.all(color: getColorBorderBtn())),
+    );
   }
 
   String getImageIconBtn() {
@@ -128,7 +254,24 @@ class _FbReactionState extends State<FbReaction> with TickerProviderStateMixin {
   Color getColorBorderBtn() {
     if ((!_isLongPress && _isLike)) {
       return Color(0xff3b5998);
-    } else {
+    }
+    else if (!_isDragging) {
+      switch (whichIconUserChoose) {
+        case 1:
+          return Color(0xff3b5998);
+        case 2:
+          return Color(0xffED5167);
+        case 3:
+        case 4:
+        case 5:
+          return Color(0xffFFD96A);
+        case 6:
+          return Color(0xffF6876B);
+        default:
+          return Colors.grey;
+      }
+    }
+    else {
       return Colors.grey[400];
     }
   }
@@ -168,6 +311,14 @@ class _FbReactionState extends State<FbReaction> with TickerProviderStateMixin {
 
   void showBow() {
     print("showBow");
+
+    playSound('box_up.mp3');
+    _isLongPress = true;
+
+    _animControlBtnLongPress.forward();
+
+    //setForwardValue();
+    animControlBox.forward();
   }
 
   double handleOutputRnageTiltIconLike(double value) {
@@ -190,11 +341,11 @@ class _FbReactionState extends State<FbReaction> with TickerProviderStateMixin {
     }
   }
 
-  Future playSound(String fileName) async{
+  Future playSound(String fileName) async {
     await _audioPlayer.stop();
     final file = File('${(await getTemporaryDirectory()).path}/$fileName');
     await file.writeAsBytes((await loadAssets(fileName)).buffer.asUint8List());
-    await _audioPlayer.play(file.path,isLocal: true);
+    await _audioPlayer.play(file.path, isLocal: true);
   }
 
   Future loadAssets(String fileName) async {
